d1eff5c01233d9d390909bd0cc7a4f2dc9b107fc
Kalebe Alves <kalebeapsoares@gmail.com>
2013-11-29 14:50:53 -0200

    Spacing

--- new_boolean.rb
+++ new_boolean.rb
@@ -1,27 +1,27 @@
 module Konditional
-    def if_then(&block)
-        self ? block.class == Proc ? block.call : self : self
-    end
+  def if_then(&block)
+    self ? block.class == Proc ? block.call : self : self
+  end
 end
 
 module MethodMissing
-    def method_missing(method, *args, &block)
-        method == :else ? block.call : super(method, *args)
-    end
+  def method_missing(method, *args, &block)
+    method == :else ? block.call : super(method, *args)
+  end
 end
 
 class TrueClass
-    include Konditional
+  include Konditional
 end
 
 class FalseClass
-    include Konditional
+  include Konditional
 
-    def else
-        yield
-    end
+  def else
+    yield
+  end
 end
 
 class Object
-    include MethodMissing
+  include MethodMissing
 end
